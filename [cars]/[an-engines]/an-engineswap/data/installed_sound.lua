0d746e2c4318763aea3f331237bc353bfe2d1ccc
oneliferpza <oneliferpza@gmail.com>
2025-11-04 22:54:10 +0200

    update

--- [cars]/[an-engines]/an-engineswap/data/installed_sound.lua
+++ [cars]/[an-engines]/an-engineswap/data/installed_sound.lua
@@ -1,99 +1,19 @@
 return {
-	["80KVC650"] = {
-	    exhaust = "wrxrllyeng",
-	    category = "car",
-	},
-	["01TEQ999"] = {
-	    exhaust = "mbnzc63eng",
-	    category = "car",
-	},
-	["69DGF576"] = {
-	    exhaust = "aq31maz13btune",
-	    category = "car",
-	},
-	["85XYP215"] = {
-	    exhaust = "ta488f154",
-	    category = "car",
-	},
-	["25KFV084"] = {
-	    exhaust = "harleyvtwin",
-	    category = "motorcycle",
-	},
-	["22OEY318"] = {
-	    exhaust = "lg50ftypev8",
-	    category = "car",
-	},
-	["83ZMR358"] = {
-	    exhaust = "taaud40v8",
-	    category = "car",
-	},
-	["20ELJ509"] = {
-	    exhaust = "cummins5924v",
-	    category = "car",
-	},
-	["25PQX315"] = {
-	    exhaust = "kc68chevy350l48",
-	    category = "car",
-	},
-	["20AGH518"] = {
-	    exhaust = "gt3rstun",
-	    category = "car",
-	},
-	["02RIA631"] = {
-	    exhaust = "audiwx",
-	    category = "car",
-	},
-	["25ATF386"] = {
-	    exhaust = "honf1v6eng",
-	    category = "car",
-	},
-	["83SNG680"] = {
-	    exhaust = "ta028viper",
-	    category = "car",
-	},
-	["25CZJ875"] = {
-	    exhaust = "n4g63t",
+	["67WNS782"] = {
+	    exhaust = "a80ffeng",
 	    category = "car",
 	},
 	["03LRF503"] = {
 	    exhaust = "lg86fer812sf",
 	    category = "car",
 	},
-	["22PBJ767"] = {
-	    exhaust = "lg68ls3vette",
+	["06OLC898"] = {
+	    exhaust = "subaruej20",
 	    category = "car",
 	},
-	["83ZDN808"] = {
-	    exhaust = "m297zonda",
-	    category = "car",
-	},
-	["43MLR467"] = {
-	    exhaust = "lg157por911",
-	    category = "car",
-	},
-	["84DBW689"] = {
-	    exhaust = "aston59v12",
-	    category = "car",
-	},
-	["69EWI588"] = {
-	    exhaust = "ta028viper",
-	    category = "car",
-	},
-	["60NDR234"] = {
-	    exhaust = "aq46forgtebv6",
-	    category = "car",
-	},
-	["26FPX883"] = {
-	    exhaust = "a80ffeng",
-	    category = "car",
-	},
-	["03LIP534"] = {
-	    exhaust = "lg157por911",
-	    category = "car",
-	},
-	["82EZT781"] = {
-	    exhaust = "lg125mnsrybently",
-	    category = "car",
+	["46KFA059"] = {
+	    exhaust = "bmws1krreng",
+	    category = "motorcycle",
 	},
 	["65JLE315"] = {
 	    exhaust = "ta488f154",
@@ -103,8 +23,152 @@ return {
 	    exhaust = "tacumminsb",
 	    category = "car",
 	},
-	["46MPG554"] = {
-	    exhaust = "aq14nisvq37vhrt",
+	["02RIA631"] = {
+	    exhaust = "audiwx",
+	    category = "car",
+	},
+	["60NDR234"] = {
+	    exhaust = "aq46forgtebv6",
+	    category = "car",
+	},
+	["83ZMR358"] = {
+	    exhaust = "taaud40v8",
+	    category = "car",
+	},
+	["25SII291"] = {
+	    exhaust = "aq31maz13btune",
+	    category = "car",
+	},
+	["84DBW689"] = {
+	    exhaust = "aston59v12",
+	    category = "car",
+	},
+	["20ELJ509"] = {
+	    exhaust = "cummins5924v",
+	    category = "car",
+	},
+	["03LIP534"] = {
+	    exhaust = "lg157por911",
+	    category = "car",
+	},
+	["83SNG680"] = {
+	    exhaust = "ta028viper",
+	    category = "car",
+	},
+	["69EWI588"] = {
+	    exhaust = "ta028viper",
+	    category = "car",
+	},
+	["00GLY025"] = {
+	    exhaust = "apollosv8",
+	    category = "car",
+	},
+	["08HYS200"] = {
+	    exhaust = "kc34merc190e",
+	    category = "car",
+	},
+	["49IIK018"] = {
+	    exhaust = "rotary7",
+	    category = "car",
+	},
+	["20AGH518"] = {
+	    exhaust = "gt3rstun",
+	    category = "car",
+	},
+	["01TEQ999"] = {
+	    exhaust = "mbnzc63eng",
+	    category = "car",
+	},
+	["45MSH874"] = {
+	    exhaust = "b58b30",
+	    category = "car",
+	},
+	["83KAY452"] = {
+	    exhaust = "bmws1krreng",
+	    category = "motorcycle",
+	},
+	["42MUD101"] = {
+	    exhaust = "wrxrllyeng",
+	    category = "car",
+	},
+	["82EZT781"] = {
+	    exhaust = "lg125mnsrybently",
+	    category = "car",
+	},
+	["22OEY318"] = {
+	    exhaust = "lg50ftypev8",
+	    category = "car",
+	},
+	["43MLR467"] = {
+	    exhaust = "lg157por911",
+	    category = "car",
+	},
+	["60YJL808"] = {
+	    exhaust = "tacumminsb",
+	    category = "car",
+	},
+	["25KFV084"] = {
+	    exhaust = "harleyvtwin",
+	    category = "motorcycle",
+	},
+	["85SMT995"] = {
+	    exhaust = "a80ffeng",
+	    category = "car",
+	},
+	["25PQX315"] = {
+	    exhaust = "kc68chevy350l48",
+	    category = "car",
+	},
+	["44QUE772"] = {
+	    exhaust = "aqtoy2jzstock",
+	    category = "car",
+	},
+	["00WDM825"] = {
+	    exhaust = "lg157por911",
+	    category = "car",
+	},
+	["83ZDN808"] = {
+	    exhaust = "m297zonda",
+	    category = "car",
+	},
+	["63SVQ186"] = {
+	    exhaust = "audr8tteng",
+	    category = "car",
+	},
+	["82GGY937"] = {
+	    exhaust = "nisgtr35",
+	    category = "car",
+	},
+	["69DGF576"] = {
+	    exhaust = "aq31maz13btune",
+	    category = "car",
+	},
+	["80HCL286"] = {
+	    exhaust = "harleyvtwin",
+	    category = "motorcycle",
+	},
+	["85XYP215"] = {
+	    exhaust = "ta488f154",
+	    category = "car",
+	},
+	["29KRR733"] = {
+	    exhaust = "taaud40v8",
+	    category = "car",
+	},
+	["04TYG639"] = {
+	    exhaust = "amg1eng",
+	    category = "car",
+	},
+	["20YKV330"] = {
+	    exhaust = "subaruej20",
+	    category = "car",
+	},
+	["03XDL498"] = {
+	    exhaust = "zr250",
+	    category = "car",
+	},
+	["07MUX919"] = {
+	    exhaust = "lg102por992ts",
 	    category = "car",
 	},
 	["86GAI205"] = {
@@ -115,100 +179,36 @@ return {
 	    exhaust = "subaruej20",
 	    category = "car",
 	},
-	["07MUX919"] = {
-	    exhaust = "lg102por992ts",
+	["80KVC650"] = {
+	    exhaust = "wrxrllyeng",
 	    category = "car",
 	},
-	["03XDL498"] = {
-	    exhaust = "zr250",
-	    category = "car",
-	},
-	["20YKV330"] = {
-	    exhaust = "subaruej20",
-	    category = "car",
-	},
-	["29KRR733"] = {
-	    exhaust = "taaud40v8",
-	    category = "car",
-	},
-	["44QUE772"] = {
-	    exhaust = "aqtoy2jzstock",
-	    category = "car",
-	},
-	["85SMT995"] = {
+	["26FPX883"] = {
 	    exhaust = "a80ffeng",
 	    category = "car",
 	},
-	["46KFA059"] = {
-	    exhaust = "bmws1krreng",
-	    category = "motorcycle",
-	},
-	["04TYG639"] = {
-	    exhaust = "amg1eng",
-	    category = "car",
-	},
-	["67WNS782"] = {
-	    exhaust = "a80ffeng",
-	    category = "car",
-	},
-	["80HCL286"] = {
-	    exhaust = "harleyvtwin",
-	    category = "motorcycle",
-	},
-	["82GGY937"] = {
-	    exhaust = "nisgtr35",
-	    category = "car",
-	},
-	["63SVQ186"] = {
-	    exhaust = "audr8tteng",
-	    category = "car",
-	},
-	["24GPI918"] = {
-	    exhaust = "ta008mustang69",
-	    category = "car",
-	},
-	["00WDM825"] = {
-	    exhaust = "lg157por911",
-	    category = "car",
-	},
-	["60YJL808"] = {
-	    exhaust = "tacumminsb",
-	    category = "car",
-	},
-	["06OLC898"] = {
-	    exhaust = "subaruej20",
+	["25ATF386"] = {
+	    exhaust = "honf1v6eng",
 	    category = "car",
 	},
 	["45JSB406"] = {
 	    exhaust = "taaud40v8",
 	    category = "car",
 	},
-	["25SII291"] = {
-	    exhaust = "aq31maz13btune",
+	["46MPG554"] = {
+	    exhaust = "aq14nisvq37vhrt",
 	    category = "car",
 	},
-	["42MUD101"] = {
-	    exhaust = "wrxrllyeng",
+	["22PBJ767"] = {
+	    exhaust = "lg68ls3vette",
 	    category = "car",
 	},
-	["45MSH874"] = {
-	    exhaust = "b58b30",
+	["24GPI918"] = {
+	    exhaust = "ta008mustang69",
 	    category = "car",
 	},
-	["00GLY025"] = {
-	    exhaust = "apollosv8",
-	    category = "car",
-	},
-	["83KAY452"] = {
-	    exhaust = "bmws1krreng",
-	    category = "motorcycle",
-	},
-	["49IIK018"] = {
-	    exhaust = "rotary7",
-	    category = "car",
-	},
-	["08HYS200"] = {
-	    exhaust = "kc34merc190e",
+	["25CZJ875"] = {
+	    exhaust = "n4g63t",
 	    category = "car",
 	},
 }

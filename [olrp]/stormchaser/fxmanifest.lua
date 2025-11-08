b3050a9d01b398aa885aeb58a9fa4f810cf753de
Stiles-Stilss <stiles.stils@gmail.com>
2025-11-08 02:27:49 +0200

    Update fxmanifest.lua

--- [olrp]/stormchaser/fxmanifest.lua
+++ [olrp]/stormchaser/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-author 'Stormchaser System'
+author 'OLRP Development'
 description 'Dynamic storm chasing gameplay for Qbox/QBCore servers'
 version '0.1.0'
 

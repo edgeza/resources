f4c9596e598c53a554ceff9f5ad3d7bdb2a931f0
oneliferpza <oneliferpza@gmail.com>
2025-11-08 09:37:12 +0200

    update

---
+++ [mlos]/DoItDigital_Legion_Square_Corner_Billboard/fxmanifest.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+fx_version 'cerulean'
+author 'KilluaZoldyck#0099'
+description 'DoItDigital Legion Suare Corner Billboard'
+version '1.0.0'
+this_is_a_map 'yes'
+games { 'gta5' }
+dependency '/assetpacks'
+
+escrow_ignore {
+    'stream/[lod]/*.*',
+}
+
+
+dependency '/assetpacks'

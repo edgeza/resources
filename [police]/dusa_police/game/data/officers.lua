1d5716908e34fc26f3189833c4c87fc2dc3ae65d
oneliferpza <oneliferpza@gmail.com>
2025-11-16 14:33:56 +0200

    update

--- [police]/dusa_police/game/data/officers.lua
+++ [police]/dusa_police/game/data/officers.lua
@@ -1,31 +1,6 @@
 return { 
-        ['R43YZ2VK'] = {
-            callsign = 'BC-502',
-            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
-        },
-    
-        ['H4E96GW5'] = {
-            callsign = 'TEST',
-            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
-        },
-    
         ['V7GEN83X'] = {
             callsign = '401',
-            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
-        },
-    
-        ['R146CI3L'] = {
-            callsign = '201 Tobani Bongani',
-            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
-        },
-    
-        ['D41RGMD7'] = {
-            callsign = 'Bc-502',
-            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
-        },
-    
-        ['TN70EJ35'] = {
-            callsign = '.LSPD 405',
             image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
         },
     
@@ -34,13 +9,48 @@ return {
             image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
         },
     
+        ['R146CI3L'] = {
+            callsign = '201 Tobani Bongani',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
+        ['H4E96GW5'] = {
+            callsign = 'TEST',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
+        ['TN70EJ35'] = {
+            callsign = '.LSPD 405',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
         ['QIU05074'] = {
             callsign = '[301] K. Kruger',
             image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
         },
     
+        ['R43YZ2VK'] = {
+            callsign = 'BC-502',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
         ['L5E9MTFV'] = {
             callsign = '31 69',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
+        ['D41RGMD7'] = {
+            callsign = 'Bc-502',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
+        ['FAN04528'] = {
+            callsign = '403 Frikkie van Tonder',
+            image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
+        },
+    
+        ['DDP94911'] = {
+            callsign = 'Jaden Carter',
             image = 'https://i.ibb.co/M8HpjY9/yigitmisin-gta-5-los-santos-police-department-officer-avatar-9332aa68-d499-49a4-819c-a932ecc1c5b5.webp'
         },
     }
